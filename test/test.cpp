59198e38cb92b84f66145cf01bc2c8651520e43a
alun0511 <unenge.alfred@gmail.com>
2026-05-13 09:27:22 +0200

    interface and esp32s3 header and source files for pin_manager

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
 
 #include "driver/adc/stub.h"
+#include "system/pin_manager/esp32s3.h"
 
 int main()
 {
+    system::pin_manager::Esp32s3::instance().isPinBusy(5ULL);
     driver::adc::Stub adc{};
     adc.simulateInput(100U);
     std::printf("ADC input: %u!\n", adc.read());
