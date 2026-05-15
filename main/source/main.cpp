6d44b2345b61a89701cf04c2eee012afd10d2283
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-15 22:27:25 +0200

    removed ugly main and added an simple timer test

--- main/source/main.cpp
+++ main/source/main.cpp
@@ -1,23 +1,21 @@
-#include <memory>
 #include "driver/timer/esp32s3.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-extern "C" void app_main() {
-    
-    
-    auto timer = std::make_unique<driver::timer::Esp32s3>();
 
-    if (timer->isInitialized()) {
-        timer->setPeriod(1000);
-        timer->start();
-        
-        // TEST
-        while (true) {
-            if (timer->isTimeout()) {
-                printf("Timeout triggered!\n");
-            }
-        
-            vTaskDelay(pdMS_TO_TICKS(10)); 
+extern "C" void app_main(void)
+{
+    // test kod bara för att ha något i main innan vi har gjort factory och logic
+    driver::timer::Esp32s3 testTimer;
+    testTimer.setPeriod(1000);
+    testTimer.start();
+
+    while (true)
+    {
+        if (testTimer.isTimeout())
+        {
+            ESP_LOGI("TEST", "Timer is working!");
         }
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
