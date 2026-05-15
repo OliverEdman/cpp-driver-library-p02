9c02ffee5e79411e59b33d56bdcf69e1f50c16e1
Maher Darwesh <mdarwish925@gmail.com>
2026-05-15 11:46:32 +0200

    Resolved merge conflicts

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+#include "driver/adc/stub.h"
+
+int main()
+{
+    driver::adc::Stub adc{};
+    adc.init();
+    adc.simulateInput(230U);
+    std::printf("ADC input: %u!\n", adc.read());
+}
