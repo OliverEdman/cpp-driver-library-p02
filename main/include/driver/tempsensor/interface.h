c2e208b6b38ac71ef809b239c4c0921ad886f9cf
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-15 14:21:50 +0200

    update interface to match requirements and added Doxygen comments

--- main/include/driver/tempsensor/interface.h
+++ main/include/driver/tempsensor/interface.h
@@ -2,14 +2,33 @@
 
 namespace driver::tempsensor {
 
+/**
+ * @class Interface
+ * @brief Abstraction for a temperature sensor driver.
+ */
+class Interface {
+public:
 
-	class Interface {
-	public:
-	    virtual ~Interface() = default;
-	}
+    /**
+    * @brief Virtual destructor.
+    */
 
-	virtual float temperature() noexcept = 0;
-	virtual bool isInitialized() noexcept = 0;
+    virtual ~Interface() = default;
+	
+    /**
+     * @brief Read the current temperature.
+     * @return Temperature in degrees Celsius.
+     */
+
+    virtual float readCelsius() noexcept = 0;
+
+    /**
+     * @brief Checks if the sensor driver is succuessfully initialized.
+     * @return true if initialized, false oterwise.
+     */
+
+    virtual bool isInitialized() const noexcept = 0;
+};
 
 } // namespace driver::tempsensor
 
