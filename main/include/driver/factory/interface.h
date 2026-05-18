dad59001197e0d268d80bfe0a6a8c75fca704838
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-18 21:03:26 +0200

    add Doxygen comments

--- main/include/driver/factory/interface.h
+++ main/include/driver/factory/interface.h
@@ -17,28 +17,77 @@ namespace driver {
 
 
 namespace driver::factory {
+
+/**
+ * @brief Abstract Factory interface for creating drivers.
+ * */
 	
 class Interface {
 
 protected: 
+
+	/**
+	 * @brief Protected constructor to allow inheritance but prevent creating objects directly.
+	 * */
 	Interface() noexcept = default;
+
 public:
+    /** 
+     * @brief Virtual Destructor.
+     */
     
     virtual ~Interface() noexcept = default;
 
+    /**
+     * @brief Create a GPIO driver instance.
+     * * @param[in] pin the GPIO pin number.
+     * @param[in] direction The data direction (Input, InputPullup, Output).
+     * @return A smart pointer to the created GPIO interface
+     */
+
     virtual std::unique_ptr<gpio::Interface> gpio(std::uint8_t pin, gpio::Direction direction) noexcept = 0;
+
+    /**
+     * @brief Create an ADC driver instance.
+     * * @return A smart pointer to the created ADC interface.
+     */
+
     virtual std::unique_ptr<adc::Interface> adc() noexcept = 0;
+
+    /**
+     * @brief Create a Serial (UART) driver instance.
+     * * @return A smart pointer to the created Serial interface.
+     *
+     * */
+
+
     virtual std::unique_ptr<serial::Interface> serial() noexcept = 0;
+
+    /** 
+     * @brief Create a Timer driver instance.
+     * * @return A smart pointer to the createdd Timer interface.
+     */
+
+
     virtual std::unique_ptr<timer::Interface> timer() noexcept = 0;
+
+    /**
+     * @brief Create a Temperature driver instance.
+     * * @param[in] pin the Pin number where the TMP36 sensor is connected.
+     * @param [in] adc A refrence to the adc instance used for reading voltage (Dependency Injection).
+     * @return A smart pointer to the created Temperature sensor interface.
+     * */
+
+
     virtual std::unique_ptr<tempsensor::Interface> tempsensor(std::uint8_t pin, adc::Interface& adc) noexcept = 0;
+
+
 
 	// NO COPY MOVE 
     Interface(const Interface&) = delete;
     Interface& operator=(const Interface&) = delete;
     Interface(Interface&&) = delete;
     Interface& operator=(Interface&&) = delete;
-
-
 
 };
 
