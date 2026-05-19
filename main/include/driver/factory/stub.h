3404bf1e969e952e50cb3601a6ca22f46e339074
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-19 02:37:07 +0200

    updated stub to match new interface

--- main/include/driver/factory/stub.h
+++ main/include/driver/factory/stub.h
@@ -9,12 +9,6 @@
 #include "driver/timer/stub.h"
 #include "driver/tempsensor/stub.h"
 
-
-namespace driver::gpio {
-    enum class Direction : std::uint8_t;
-    class Interface;
-}
-
 namespace driver::factory {
 
 class Stub final : public Interface {
@@ -23,55 +17,40 @@ public:
     Stub() noexcept = default;
     ~Stub() noexcept override = default;
 
-    /**
-     * @brief Create a GPIO driver instance.
-     * @param[in] pin The GPIO pin number.
-     * @param[in] direction The pin direction (Input/Output).
-     * @return nullptr since the GPIO driver is not implemented yet.
-     */
-
-    std::unique_ptr<gpio::Interface> gpio(std::uint8_t pin, gpio::Direction direction) noexcept override {
+    /** @return nullptr since the GPIO driver is not implemented yet. */
+    std::unique_ptr<gpio::Interface> gpioInput(std::uint8_t pin) noexcept override {
         (void)pin;
-        (void)direction;
         return nullptr; 
     }
 
-    /**
-     * @brief Create an ADC driver instance.
-     * @return A smart pointer to the created ADC interface.
-     */
+    /** @return nullptr since the GPIO driver is not implemented yet. */
+    std::unique_ptr<gpio::Interface> gpioOutput(std::uint8_t pin) noexcept override {
+        (void)pin;
+        return nullptr; 
+    }
 
+    /** @return A smart pointer to the created ADC interface. */
     std::unique_ptr<adc::Interface> adc() noexcept override {
         return std::make_unique<driver::adc::Stub>();
     }
 
-    /**
-     * @brief Create a Serial (UART) driver instance.
-     * @return A smart pointer to the created Serial interface.
-     */
-
-    std::unique_ptr<serial::Interface> serial() noexcept override {
+    /** @return A smart pointer to the created Serial interface. */
+    std::unique_ptr<serial::Interface> serial(std::uint32_t baud_bps) noexcept override {
+        (void)baud_bps;
         return std::make_unique<driver::serial::Stub>();
     }
 
-    /** * @brief Create a Timer driver instance.
-     * @return A smart pointer to the created Timer interface.
-     */
-
-    std::unique_ptr<timer::Interface> timer() noexcept override {
-        return std::make_unique<driver::timer::Stub>(); 
+    /** @return A smart pointer to the created Temperature sensor interface. */
+    std::unique_ptr<tempsensor::Interface> tempSensor(std::uint8_t pin, adc::Interface& adc) noexcept override {
+        (void)pin;
+        (void)adc;
+        return std::make_unique<driver::tempsensor::Stub>();
     }
 
-    /**
-     * @brief Create a Temperature driver instance.
-     * @param[in] pin the Pin number where the TMP36 sensor is connected.
-     * @param[in] adc A reference to the adc instance used for reading voltage (Dependency Injection).
-     * @return A smart pointer to the created Temperature sensor interface.
-     */
-
-    std::unique_ptr<tempsensor::Interface> tempsensor(std::uint8_t pin, adc::Interface& adc) noexcept override {
-        (void)pin;
-        return std::make_unique<driver::tempsensor::Stub>(adc);
+    /** @return A smart pointer to the created Timer interface. */
+    std::unique_ptr<timer::Interface> timer(std::uint16_t timeout_ms) noexcept override {
+        (void)timeout_ms;
+        return std::make_unique<driver::timer::Stub>(); 
     }
 
     // No copy and move
@@ -80,4 +59,5 @@ public:
     Stub(Stub&&)                 = delete;
     Stub& operator=(Stub&&)      = delete;
 };
+
 } // namespace driver::factory
