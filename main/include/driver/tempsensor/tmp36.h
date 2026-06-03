a0482bdf25bf439b27e045d0533fe58836fd81a6
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-03 19:48:54 +0200

    added documentation for the tempsensor module

--- main/include/driver/tempsensor/tmp36.h
+++ main/include/driver/tempsensor/tmp36.h
@@ -1,6 +1,6 @@
 /**
- * @file Tmp36.h
- * @brief Real driver implementation for the TMP36 temperature sensor.
+ * @file tmp36.h
+ * @brief Real driver interface for the TMP36 temperature sensor.
  */
 
 #pragma once
@@ -13,57 +13,50 @@ namespace driver::tempsensor {
 
 
 /**
- * @class Tmp36
  * @brief TMP36 temperature sensor driver using ADC Dependency Injection.
- *
  * Implements the temperature interface by converting analog voltage readings
  * from an injected ADC instance into Celsius degrees.
+ * This class cannot be copied or moved.
  */
-
 class Tmp36 final : public Interface {
 public:
-
-
-
      /**
      * @brief Constructor.
-     * @param adc Reference to the ADC interface used to get sensor voltage.
+     * @param[in] adc Reference to the ADC interface used to get sensor voltage.
+     * @param[in] pin the specific ADC pin/channel number of the sensor is connected to.
      */
-
    explicit Tmp36(driver::adc::Interface& adc, std::uint8_t pin) noexcept; 
 
     /**
      * @brief Destructor.
      */
-
     ~Tmp36() noexcept override = default;
 
     /**
-     * @brief Reads the current temperature from the hardware.
-     * * Converts the analog voltage read from the ADC into degrees Celsius
-     * Using (Voltage - 0.5V) * 100.
-     * * @return Current temperature in degrees Celsius.
+     * @brief Read the current temperature from the hardware.
+     * Converts the analog voltage read from the ADC into degrees Celsius
+     * Using the formula: (Voltage - 0.5V) * 100.
+     * @return The current temperature value in degrees Celsius.
      */
     float readCelsius() noexcept override;
 
     /**
-     * @brief Checks if the temperature sensor is ready to be used.      
-     * @return true if initialized and ready, false otherwise.
+     * @brief Check if the temperature sensor is ready to be used.      
+     * @return True if initialized and ready, false otherwise.
      */
     bool isInitialized() const noexcept override;
 
-    // Disable Copy and Move operations
+    // Delete copy/move constructors and operators.
     Tmp36(const Tmp36&)            = delete;
     Tmp36& operator=(const Tmp36&) = delete;
     Tmp36(Tmp36&&)                 = delete;
     Tmp36& operator=(Tmp36&&)      = delete; 
 
 private:
-    /** 
-     * @brief Reference to the injected ADC interface. 
-     */
-
+    /** @brief Refrence to the injected ADC interface.*/
     driver::adc::Interface& myAdc;
+
+    /** @brief The hardware pin number assigned to this sensor*/
     std::uint8_t myPin;
 };
 
