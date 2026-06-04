6d8b5c5d5832394a7450c87c9b75d5257b885d33
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-04 15:08:58 +0200

    removed magic numbers by using consexpr

--- main/source/driver/tempsensor/tmp36.cpp
+++ main/source/driver/tempsensor/tmp36.cpp
@@ -1,6 +1,15 @@
 #include "driver/tempsensor/tmp36.h"
 
-namespace driver::tempsensor {
+namespace driver::tempsensor
+{
+
+namespace 
+{
+
+constexpr float VoltageOffset{0.5f};
+constexpr float DegreesPerVolt{100.0f};
+
+} // namespace
 
 Tmp36::Tmp36(driver::adc::Interface& adc, std::uint8_t pin) noexcept 
     : myAdc{adc}
@@ -17,7 +26,7 @@ float Tmp36::readCelsius() noexcept {
     // Convert voltage to Celsius using the the following formula:
     // (Voltage - 500mV offset) * 100 degrees/V
     // Tmp36 have a offset of 0.5v so we must subtract 0.5V from the output voltage. (500mV)
-    const auto temp = (voltage - 0.5f) * 100.0f;
+    const auto temp = (voltage - VoltageOffset) * DegreesPerVolt;
 
     return temp;
 }
