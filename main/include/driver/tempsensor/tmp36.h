a4172caa0e2b4adc2aeb23ffe408eef7ffec7971
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-16 22:44:20 +0200

    fixed changes from feedback

--- main/include/driver/tempsensor/tmp36.h
+++ main/include/driver/tempsensor/tmp36.h
@@ -1,13 +1,14 @@
 /**
- * @file tmp36.h
  * @brief Real driver interface for the TMP36 temperature sensor.
  */
 
 #pragma once
 
-#include "driver/tempsensor/interface.h"
+#include <cstdint>
+
 #include "driver/adc/interface.h"
 
+#include "driver/tempsensor/interface.h"
 
 namespace driver::tempsensor {
 
@@ -18,12 +19,14 @@ namespace driver::tempsensor {
  * from an injected ADC instance into Celsius degrees.
  * This class cannot be copied or moved.
  */
-class Tmp36 final : public Interface {
+class Tmp36 final : public Interface
+{
 public:
      /**
      * @brief Constructor.
+     *
      * @param[in] adc Reference to the ADC interface used to get sensor voltage.
-     * @param[in] pin the specific ADC pin/channel number of the sensor is connected to.
+     * @param[in] pin The specific ADC pin/channel number of the sensor is connected to.
      */
    explicit Tmp36(driver::adc::Interface& adc, std::uint8_t pin) noexcept; 
 
@@ -32,16 +35,19 @@ public:
      */
     ~Tmp36() noexcept override = default;
 
-    /**
+     /**
      * @brief Read the current temperature from the hardware.
-     * Converts the analog voltage read from the ADC into degrees Celsius
-     * Using the formula: (Voltage - 0.5V) * 100.
+     * 
+     *        Converts the analog voltage read from the ADC into degrees Celsius
+     *        using the formula: (Voltage - 0.5V) * 100.
+     * 
      * @return The current temperature value in degrees Celsius.
      */
     float readCelsius() noexcept override;
 
     /**
-     * @brief Check if the temperature sensor is ready to be used.      
+     * @brief Check if the temperature sensor is ready to be used.
+     *
      * @return True if initialized and ready, false otherwise.
      */
     bool isInitialized() const noexcept override;
@@ -53,10 +59,10 @@ public:
     Tmp36& operator=(Tmp36&&)      = delete; 
 
 private:
-    /** @brief Refrence to the injected ADC interface.*/
+    /** Refrence to the injected ADC interface.*/
     driver::adc::Interface& myAdc;
 
-    /** @brief The hardware pin number assigned to this sensor*/
+    /** The hardware pin number assigned to this sensor*/
     std::uint8_t myPin;
 };
 
